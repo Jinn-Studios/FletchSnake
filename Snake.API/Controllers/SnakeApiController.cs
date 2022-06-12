7c9c99250dfb906ef0f5540b58ea1b34368ed57c
Fletch <steven@jinnstudios.com>
2022-06-12 16:37:25 -0500

    Add game end endpoint

--- Snake.API/Controllers/SnakeApiController.cs
+++ Snake.API/Controllers/SnakeApiController.cs
@@ -7,17 +7,19 @@ namespace Snake.API
     public class SnakeApiController : ControllerBase
     {
         [HttpGet]
-        public Task<APIDetailsDTO> GetSomething()
+        public Task<APIDetailsDTO> GetAPIDetails()
             => Task.FromResult(new APIDetailsDTO());
 
         [HttpPut("start")]
         public Task StartNewGame([FromBody] InstanceDTO gameBoardDetails)
-        {
-            return Task.CompletedTask;
-        }
+            => Task.CompletedTask;
 
         [HttpPost("move")]
         public Task<MoveDTO> MakeAMove([FromBody] InstanceDTO gameBoardDetails)
             => Task.FromResult(new MoveDTO { move = "up" });
+
+        [HttpPost("end")]
+        public Task EndGame([FromBody] InstanceDTO gameBoardDetails)
+            => Task.CompletedTask;
     }
 }
